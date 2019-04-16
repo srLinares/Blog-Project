1410b3f58b888b9c539886ec9f95f91645af1780
srLinares <49556602+srLinares@users.noreply.github.com>
2019-04-16 11:14:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -152,6 +152,8 @@ $(document).ready(function() {
 		nav: false,
       	navspeed: 300,
       	autoplay: true,
+      	autoplayTimeout:2000,
+		autoplayHoverPause: true
 		responsive: {
 			0: {
 				items: 1
@@ -186,6 +188,7 @@ $(document).ready(function() {
 		nav: false,
       	navspeed: 650,
       	autoplay: true,
+      	autoplayHoverPause: true,
 		responsive: {
 			0: {
 				items: 1
